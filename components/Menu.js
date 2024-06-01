e07e1792a4346c19a11da17d3109ddb5d9ac470e
Abraham Gracco <159852551+abrahamgracco@users.noreply.github.com>
2024-06-01 23:45:09 +0000

    adiciona arquivos de manifesto corrigido

---
+++ components/Menu.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+const Menu = () => (
+  <div>
+    <a>Welcome to GRACCO TECHNOLOGIES INC.</a>
+  </div>
+);
+
+export default Menu;
---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Menu from "../components/Menu";
+
+function Home() {
+  return (
+    <div>
+      <Head>
+        <meta SharSet="utf-8" />
+        <meta name="robots" content="index, follow" />
+        <meta name="description" content="Site about Quantum Tecnlogies" />
+        <meta name="author" content="Abraham Gracco" />
+        <meta
+          name="viewpoint"
+          content="initial-scale=1.0, width=devide-width"
+        />
+        <title> Abraão Gracco - Home </title>
+      </Head>
+      <Menu />
+      Conteúdo do topo do site
+    </div>
+  );
+}
+
+export default Home;
